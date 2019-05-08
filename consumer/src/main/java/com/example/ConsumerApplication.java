6091ee87b26ab3c8739a3a8d2a405b53180be439
Dawn James <dawn.james@kobaltmusic.com>
2019-05-08 16:48:37 +0100

    Producer and consumer are now working with Avro.

--- consumer/src/main/java/com/example/ConsumerApplication.java
+++ consumer/src/main/java/com/example/ConsumerApplication.java
@@ -2,35 +2,44 @@ package com.example;
 
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.kafka.clients.consumer.*;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class ConsumerApplication {
 
-    public static String TOPIC_NAME = "test";
+    public static String TOPIC_NAME = "transactions";
 
     public static void main(String[] args) {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
-        props.setProperty("group.id", "test");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-payments");
         props.setProperty("enable.auto.commit", "true");
         props.setProperty("auto.commit.interval.ms", "1000");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
         props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
 
-        final Consumer<String, GenericRecord> consumer = new KafkaConsumer<>(props);
+        final KafkaConsumer<String, Payment> consumer = new KafkaConsumer<>(props);
 
-        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
         System.out.println("Subscribed to topic " + TOPIC_NAME);
         while (true) {
-            ConsumerRecords<String, GenericRecord> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, GenericRecord> record : records)
-                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            final ConsumerRecords<String, Payment> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, Payment> record : records) {
+                final String key = record.key();
+                final Payment value = record.value();
+                System.out.printf("key = %s, value = %s%n", key, value);
+            }
         }
     }
 }
